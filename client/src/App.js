fe4029a6a74816c0bf7895840bb9d92c2a269529
Mathias231 <mathias.krogh@outlook.com>
2022-06-03 09:06:46 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import './css/App.css';
+import Navbar from './navbar/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
