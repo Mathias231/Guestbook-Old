57a831cef54cfccda84befd62655cbba18488888
Mathias231 <mathias.krogh@outlook.com>
2022-06-03 20:31:06 +0200

    Now change's button's name on button click

--- client/src/components/multiButton/Button.js
+++ client/src/components/multiButton/Button.js
@@ -1,11 +1,30 @@
 // This button shall show and close a form on press
 
 import Button from '@mui/material/Button';
+import React, { useState } from "react";
 
-function multiButton() {
+
+
+function MultiButton() {
+    const [name, setName] = useState('CLOSE');
+
+    // Change's button's parameter (name) to "CLOSE" or OPEN on button click 
+    function changeName() {
+        switch(name) {
+            case 'CLOSE':
+                setName('OPEN', name)
+                break;
+            case 'OPEN':
+                setName('CLOSE', name)
+                break;
+            default:
+                throw new Error();
+        }
+    }
+
     return (
-        <Button variant="contained">Opprett innlegg</Button>
+        <Button variant="contained" name={name} onClick={changeName}>Opprett innlegg</Button>
     )
 }
 
-export default multiButton;
+export default MultiButton;
