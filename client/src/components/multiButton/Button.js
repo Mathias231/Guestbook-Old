9df25939f905107450ee239a142991ef485cb0ef
Mathias231 <mathias.krogh@outlook.com>
2022-06-03 20:13:17 +0200

    oof

--- client/src/components/multiButton/Button.js
+++ client/src/components/multiButton/Button.js
@@ -3,16 +3,8 @@
 import Button from '@mui/material/Button';
 
 function button() {
-    let name = 'OPEN';
-    console.log(name);
-    
-    const changeName = () => {
-        name = "CLOSE"; 
-        console.log(name);
-    };
-
     return (
-        <Button variant="contained" name={name} onClick={changeName()}>Opprett innlegg</Button>
+        <Button variant="contained">Opprett innlegg</Button>
     )
 }
 
