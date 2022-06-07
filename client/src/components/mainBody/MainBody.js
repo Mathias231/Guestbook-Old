c40219c49e7c18cc48e743fa23a70dc29d10fefc
Mathias231 <mathias.krogh@outlook.com>
2022-06-07 13:35:06 +0200

    Moved button and made it's own component instead

--- client/src/components/mainBody/MainBody.js
+++ client/src/components/mainBody/MainBody.js
@@ -4,12 +4,8 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
-//Importing style and useState for button porpuse
-import Button from '@mui/material/Button';
-import { useState } from "react";
-
 // Importing components
-import Form from '../form/Form'
+import SwitchButton from '../buttons/SwitchButton'
 
 // Styling for Grid (found from grid documentation from MUI)
 const Item = styled(Paper)(({ theme }) => ({
@@ -23,23 +19,7 @@ const Item = styled(Paper)(({ theme }) => ({
 
 // Creates the "Main body" where form and posts will be inside.
 function MainBody() {
-  // Sets variable
-  const [show, setShow] = useState(false);
 
-  // Change's button's parameter (name) to "CLOSE" or OPEN on button click 
-  function changeState() {
-      switch(show) {
-          case true:
-              setShow(false, show)
-              break;
-          case false:
-              setShow(true, show)
-              break;
-          default:
-              throw new Error();
-      }
-  }
-  
     return (
      <div>
       <Box>
@@ -49,8 +29,7 @@ function MainBody() {
             </Grid>
             <Grid item xs={6} md={4}>
               <Item>
-                <Button variant="contained" value={show} onClick={changeState}>{show ? "Lukk" : "Opprett innlegg"}</Button>,
-                {show ? <Form /> : null}
+                <SwitchButton />
               </Item>
             </Grid>
           </Grid>
