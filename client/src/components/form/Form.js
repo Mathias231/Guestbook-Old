b2e21d413ccf8f101d66e18a7d4b90abd71394aa
Mathias231 <mathias.krogh@outlook.com>
2022-06-07 11:55:35 +0200

    Added TextField and Textarea to form

--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -1,23 +1,27 @@
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
 
 
 function Form() {
     return (
-    <Grid item xs={6} md={4}>
-        <Item>
-            <h1>Opprett innlegg</h1>
-        </Item>
-    </Grid>
+        <Box
+            component="form"
+            sx={{
+            '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+        >
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextareaAutosize
+            maxRows={4}
+            aria-label="empty textarea"
+            placeholder="Empty"
+            style={{ width: 200 }}
+        />
+      </Box>
     )
 }
 
