c41a4dfd186ce77abf86a1c793260477ed6a82b4
Mathias231 <mathias.krogh@outlook.com>
2022-06-07 11:55:50 +0200

    fixed script

--- client/src/components/buttons/SwitchButton.js
+++ client/src/components/buttons/SwitchButton.js
@@ -3,25 +3,29 @@
 import Button from '@mui/material/Button';
 import React, { useState } from "react";
 
-function SwitchButton() {
-    const [name, setName] = useState('CLOSE');
+// Importing components
+// import Form from '../form/Form'
+
+function SwitchButton(props) {
+    const [show, setShow] = useState();
 
     // Change's button's parameter (name) to "CLOSE" or OPEN on button click 
     function changeName() {
-        switch(name) {
-            case 'CLOSE':
-                setName('OPEN', name)
+        switch(show) {
+            case true:
+                setShow(false, show)
                 break;
-            case 'OPEN':
-                setName('CLOSE', name)
+            case false:
+                setShow(true, show)
                 break;
             default:
                 throw new Error();
         }
     }
 
+
     return (
-        <Button variant="contained" name={name} onClick={changeName}>Opprett innlegg</Button>
+        <Button variant="contained" value={name} onClick={changeName}>Opprett innlegg</Button>
     )
 }
 
