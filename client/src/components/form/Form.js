9d87f8455f155023fefefcda654a7c210cd7d774
Mathias231 <mathias.krogh@outlook.com>
2022-06-08 15:55:33 +0200

    Updated

--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -7,14 +7,23 @@ function Form() {
         <Box
             component="form"
             sx={{
-            '& > :not(style)': { m: 1, width: '30ch' },
+            '& > :not(style)': { m: 1, width: '35ch' },
             }}
             mt={1}
             noValidate
             autoComplete="off"
+            required
         >
         <h4>test</h4>
         <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <TextField
+          id="outlined-multiline-static"
+          label="Multiline"
+          multiline
+          rows={4}
+          defaultValue="Default Value"
+        />
+        <button>test</button>
       </Box>
     )
 }
