58dcda558918d948e3eb86379740915dd17875d5
Mathias231 <mathias.krogh@outlook.com>
2022-06-09 10:20:31 +0200

    Added useState so title can be saved

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField } from '@mui/material/';
 
 function Input() {
+  const [title, setTitle] = useState('');
     return(
         <TextField
         required
         id="outlined-required"
         label="Tittel"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         fullWidth
       />
     )
