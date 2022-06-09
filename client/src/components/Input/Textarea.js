a1c00ce1fa009c14ab0a15f0013ee541ed788cd3
Mathias231 <mathias.krogh@outlook.com>
2022-06-09 10:26:56 +0200

    Added value and onChange function to textarea

--- client/src/components/Input/Textarea.js
+++ client/src/components/Input/Textarea.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField } from '@mui/material/';
 
 // It is worth to mention that I'm using a TextField instead of TextareaAutosize beacuse it's component's CSS is ugly
 
 function Textarea() {
+    const [body, setBody] = useState('');
+    
     return(
         <TextField
             id="outlined-multiline-static"
@@ -11,6 +13,8 @@ function Textarea() {
             multiline
             rows={4}
             fullWidth
+            value={ body }
+            onChange={(e) => setBody(e.target.value)}
         />
     )
 }
