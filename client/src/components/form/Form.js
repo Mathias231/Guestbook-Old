d4b1f2cfa9f2b766e774936f2b8a98e61c06c66b
Mathias231 <mathias.krogh@outlook.com>
2022-06-10 12:12:33 +0200

    Updating form and removing import

--- client/src/components/form/Form.js
+++ client/src/components/form/Form.js
@@ -1,25 +1,44 @@
-import * as React from 'react';
-import { Container, Grid } from '@mui/material/';
-
-// Importing component
-import Input from '../Input/Input';
-import Textarea from '../Input/Textarea';
-import SubmitButton from '../Buttons/SubmitButton';
+import React, { useState } from 'react';
+import { Container, Grid, TextField, Button } from '@mui/material/';
 
 function Form() {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    function handleSubmit() {
+
+    }
 
     return (
         <Container>
-            <h1>Opprett Innlegg</h1>
-            <Grid mt={2}>
-                <Input />
-            </Grid>
-            <Grid mt={2}>
-                <Textarea />
-            </Grid>
-            <Grid mt={2}>
-                <SubmitButton />
-            </Grid>
+            <form onSubmit={handleSubmit()}>
+                <h1>Opprett Innlegg</h1>
+                <Grid mt={2}>
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Tittel"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    fullWidth
+                />
+                </Grid>
+                <Grid mt={2}>
+                    <TextField
+                        id="outlined-multiline-static"
+                        label="Skriv her"
+                        multiline
+                        rows={4}
+                        fullWidth
+                        value={ body }
+                        onChange={(e) => setBody(e.target.value)}
+                    />
+                </Grid>
+                <Grid mt={2}>
+                    <Button variant="contained">
+                        Opprett innlegg
+                    </Button>
+                </Grid>
+            </form>
         </Container>
     )
 }
