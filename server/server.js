44d54453ee3d567aeae1b0744bab698f116cd412
Mathias231 <mathias.krogh@outlook.com>
2022-06-13 10:10:48 +0200

    Commenting out connection and removing mongoose (temporary)

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongoose = require("mongoose");
 
 require("dotenv").config({ path: "./config.env" });
 
@@ -10,13 +9,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 
-const connection = mongoose.connect;
-connection.once('open', () => {
-  console.log("MongoDB connection success");
-})
+// const connection = mongoose.connect;
+// connection.once('open', () => {
+//   console.log("MongoDB connection success");
+// })
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
