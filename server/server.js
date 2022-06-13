d7f4b8d6e928631caff28aad4be4c0a4384722cc
Mathias231 <mathias.krogh@outlook.com>
2022-06-13 10:36:29 +0200

    Connection created

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+// Put's MongoDB connection string into variable and create connection
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri)
+mongoose.connect(uri);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
