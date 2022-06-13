42e81d872102cd4db18a04d7c2555318918e08c4
Mathias231 <mathias.krogh@outlook.com>
2022-06-13 10:18:27 +0200

    Updated

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const mongoose = require('mongoose');
 
 require("dotenv").config({ path: "./config.env" });
 
@@ -9,13 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.ATLAS_URI;
-// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-
-// const connection = mongoose.connect;
-// connection.once('open', () => {
-//   console.log("MongoDB connection success");
-// })
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri)
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
