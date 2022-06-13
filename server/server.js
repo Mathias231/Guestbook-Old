94c476541588e9cfffe7a8be3a8e93556879c75d
Mathias231 <mathias.krogh@outlook.com>
2022-06-13 12:28:12 +0200

    Updated mongoose connection with .then and .catch promises

--- server/server.js
+++ server/server.js
@@ -12,7 +12,14 @@ app.use(express.json());
 
 // Put's MongoDB connection string into variable and create connection
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri);
+
+mongoose.connect(uri)
+  // Console log success/error message
+  .then(() => console.log('MongoDB connection success'))
+  .catch(err => console.log(err));
+
+app.use('/post.js', postRouter);
+app.use('/users.js', usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
