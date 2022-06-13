f067acc016426e788203b2e81f12277e8c781077
Mathias231 <mathias.krogh@outlook.com>
2022-06-13 12:55:54 +0200

    Adding routing for get and post request

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,3 +1,23 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
+
+router.get('/').get((request, response) => {
+    User.find()
+    .then(users => response.json(users))
+    .catch(err => response.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((request, response) => {
+    const username = request.body.username;
+    const password = request.body.password;
+    const date = Date.parse(request.body.date);
+
+    const newUser = new User({username, password, date});
+    
+    newUser.save()
+    .then(() => response.json('User added!'))
+    .catch(err => response.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
