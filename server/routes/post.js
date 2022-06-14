74ee4c31acd6567ad2edb354c22e47131d35fb82
Mathias231 <mathias.krogh@outlook.com>
2022-06-14 14:30:21 +0200

    Adding routes delete, get post by id and added comments

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,12 +1,21 @@
 const router = require('express').Router();
 let Post = require('../models/post.model');
 
+// Get all posts from DB
 router.route('/').get((request, response) => {
     Post.find()
     .then(posts => response.json(posts))
     .catch(err => response.status(400).json('Error: ' + err));
 });
 
+// Get post by ID
+router.route('/:id').get((request, response) => {
+    Post.findById(request.params.id)
+    .then(posts => response.json(posts))
+    .catch(err => response.status(400).json('Error: ' + err));
+});
+
+// Add new post to DB
 router.route('/add').post((request, response) => {
     const title = request.body.title;
     const username = request.body.username;
@@ -18,6 +27,13 @@ router.route('/add').post((request, response) => {
     newPost.save()
     .then(() => response.json('Post added!'))
     .catch(err => response.status(400).json('Erreor: ' + err));
+});
+
+// Delete post by ID
+router.route('/:id').delete((request, response) => {
+    Post.findByIdAndDelete(request.params.id)
+    .then(() => response.json('Post deleted!'))
+    .catch(err => response.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
