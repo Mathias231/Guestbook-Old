520fbaf35f75dcacd7d307c5f48714e4a49d8184
Mathias231 <mathias.krogh@outlook.com>
2022-06-14 10:54:30 +0200

    Adding comment and delete route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,21 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
 
+// Get all users from DB
 router.route('/').get((request, response) => {
     User.find()
     .then(users => response.json(users))
     .catch(err => response.status(400).json('Error: ' + err));
 });
 
+// Get user by ID
 router.route('/:id').get((request, response) => {
     User.findById(request.params.id)
     .then(users => response.json(users))
     .catch(err => response.status(400).json('Error: ' + err));
-})
+});
 
+// Add new user to DB
 router.route('/add').post((request, response) => {
     const username = request.body.username;
     const password = request.body.password;
@@ -24,6 +27,11 @@ router.route('/add').post((request, response) => {
     .catch(err => response.status(400).json('Erreor: ' + err));
 });
 
-
+// Delete user by ID
+router.route('/:id').delete((request, response) => {
+    User.findIdAndDelete(request.params.id)
+    .then(() => response.json('User deleted!'))
+    .catch(err => response.status(400).json('Error: ' + err));
+});
 
 module.exports = router;
