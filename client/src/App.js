191b3ae22a51cf859f8148d8ef702775fe033d65
Mathias231 <mathias.krogh@outlook.com>
2022-06-15 14:24:59 +0200

    Added import Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
+import { Router } from "react-router-dom";
+
+// Importing components
 import Navbar from './components/Navbar/Navbar';
-import MainBody from './components/MainBody/MainBody'
+import MainBody from './components/MainBody/MainBody';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <MainBody />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <MainBody />
+      </div>
+    </Router>
   );
 }
 
