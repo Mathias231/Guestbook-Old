80a94d7cb190872c4a9494ea01916b64e0b4fbb9
Mathias231 <mathias.krogh@outlook.com>
2022-06-16 12:57:04 +0200

    Adding links

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,13 +1,14 @@
 import * as React from "react";
-import { Link } from "react-router-dom";
 import { AppBar, Box, Toolbar } from "@mui/material/";
+import { Link } from "react-router-dom";
 
 export default function ButtonAppBar() {
   return (
     <Box>
       <AppBar position="static">
         <Toolbar>
-          <Link to="/login">Login</Link>
+            <Link to="/" style={{ textDecoration: 'none', color: 'white'}}>Home</Link>
+            <Link to="../Login/" style={{ textDecoration: 'none', color: 'white'}}>Login</Link>
         </Toolbar>
       </AppBar>
     </Box>
