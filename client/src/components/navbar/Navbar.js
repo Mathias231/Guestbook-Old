859533cd275de07df99c8c259668e38df5a73a90
Mathias231 <mathias.krogh@outlook.com>
2022-06-21 10:07:56 +0200

    Changed function nam

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { AppBar, Box, Toolbar } from "@mui/material/";
 import { Link } from "react-router-dom";
 
-export default function ButtonAppBar() {
+export default function Navbar() {
   return (
     <Box>
       <AppBar position="static">
